193d0a987e25e1110e970f5616c8114eeb2636f7
Arthur Ribeiro <arthurfribeiro494@gmail.com>
2026-05-21 16:09:12 -0300

    Database renomeado para models

--- models.py
+++ models.py
@@ -17,12 +17,23 @@ class Users(Base):
     username = Column("username",String,nullable=False)
     email = Column("email",String, unique=True)
     senha = Column("senha",String,nullable=False)
+    pfp = Column("pfp",String)
+
+    def _init__(self, username, email, senha, pfp):
+        self.username = username
+        self.email = email
+        self.senha = senha
+        self.pfp = pfp
 
 class Genres(Base):
     __tablename__ = 'genres'
     id = Column("id",Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column("name",String,nullable=False)
     description = Column("description",String,nullable=False)
+
+    def _init__(self, name, description):
+        self.name = name
+        self.description = description
 
 class Covers(Base):
     __tablename__ = 'covers'
@@ -33,6 +44,13 @@ class Covers(Base):
     genre_id = Column("genre_id",Integer, ForeignKey('genres.id'), nullable=False)
     date = Column("date",String,nullable=False)
 
+    def _init__(self, name, description, image_url, genre_id, date):
+        self.name = name
+        self.description = description
+        self.image_url = image_url
+        self.genre_id = genre_id
+        self.date = date
+
 
 class Artists(Base):
     __tablename__ = 'artists'
@@ -42,6 +60,12 @@ class Artists(Base):
     image_url = Column("image_url",String,nullable=False)
     genre_id = Column("genre_id",Integer, ForeignKey('genres.id'), nullable=False)
 
+    def _init__(self, name, description, image_url, genre_id):
+        self.name = name
+        self.description = description
+        self.image_url = image_url
+        self.genre_id = genre_id
+
 class Rates(Base):
     __tablename__ = 'rates'
     id = Column("id",Integer, primary_key=True, autoincrement=True, nullable=False)
@@ -49,3 +73,9 @@ class Rates(Base):
     cover_id = Column("cover_id",Integer, ForeignKey('covers.id'), nullable=False)
     artist_id = Column("artist_id",Integer, ForeignKey('artists.id'), nullable=False)
     rating = Column("rating",Integer, nullable=False)
+
+    def _init__(self, user_id, cover_id, artist_id, rating):
+        self.user_id = user_id
+        self.cover_id = cover_id
+        self.artist_id = artist_id
+        self.rating = rating
