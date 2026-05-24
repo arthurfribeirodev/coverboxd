d22ccb7c75ef6c32fc5220fdd022978a8ccc262b
Arthur Ribeiro <arthurfribeiro494@gmail.com>
2026-05-24 18:21:20 -0300

    Mudanca para senha criptografada

--- auth_routes.py
+++ auth_routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 from models import Users,db
 from dependencies import session_grab
+from main import bcrypt_context
 
 auth_router = APIRouter(prefix="/auth",tags=["auth"])
 
@@ -11,7 +12,8 @@ async def register(username: str, email: str, senha: str, session = Depends(sess
     if user:
         return {"message": "Email já registrado"}
     else:
-        new_user = Users(username, email, senha)
+        pass_hash = bcrypt_context.hash(senha)
+        new_user = Users(username, email, pass_hash)
         session.add(new_user)
         session.commit()
         return {"message": "Usuário registrado com sucesso"}
