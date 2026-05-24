7b2ac961edb04e13973d6f3021cb5d01e07ffea4
Arthur Ribeiro <arthurfribeiro494@gmail.com>
2026-05-24 13:12:52 -0300

    Modelos de Dados alterados

--- models.py
+++ models.py
@@ -12,39 +12,39 @@ Base = declarative_base()
 # Criação de Tabelas
 
 class Users(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
     id = Column("id",Integer, primary_key=True, autoincrement=True, nullable=False)
     username = Column("username",String,nullable=False)
     email = Column("email",String, unique=True)
     senha = Column("senha",String,nullable=False)
     pfp = Column("pfp",String)
 
-    def _init__(self, username, email, senha, pfp):
+    def __init__(self, username, email, senha, pfp=None):
         self.username = username
         self.email = email
         self.senha = senha
         self.pfp = pfp
 
 class Genres(Base):
-    __tablename__ = 'genres'
+    __tablename__ = "genres"
     id = Column("id",Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column("name",String,nullable=False)
     description = Column("description",String,nullable=False)
 
-    def _init__(self, name, description):
+    def __init__(self, name, description):
         self.name = name
         self.description = description
 
 class Covers(Base):
-    __tablename__ = 'covers'
+    __tablename__ = "covers"
     id = Column("id",Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column("name",String,nullable=False)
     description = Column("description",String,nullable=False)
     image_url = Column("image_url",String,nullable=False)
-    genre_id = Column("genre_id",Integer, ForeignKey('genres.id'), nullable=False)
+    genre_id = Column("genre_id",Integer, ForeignKey("genres.id"), nullable=False)
     date = Column("date",String,nullable=False)
 
-    def _init__(self, name, description, image_url, genre_id, date):
+    def __init__(self, name, description, image_url, genre_id, date):
         self.name = name
         self.description = description
         self.image_url = image_url
@@ -58,23 +58,23 @@ class Artists(Base):
     name = Column("name",String,nullable=False)
     description = Column("description",String,nullable=False)
     image_url = Column("image_url",String,nullable=False)
-    genre_id = Column("genre_id",Integer, ForeignKey('genres.id'), nullable=False)
+    genre_id = Column("genre_id",Integer, ForeignKey("genres.id"), nullable=False)
 
-    def _init__(self, name, description, image_url, genre_id):
+    def __init__(self, name, description, image_url, genre_id):
         self.name = name
         self.description = description
         self.image_url = image_url
         self.genre_id = genre_id
 
 class Rates(Base):
-    __tablename__ = 'rates'
+    __tablename__ = "rates"
     id = Column("id",Integer, primary_key=True, autoincrement=True, nullable=False)
     user_id = Column("user_id",Integer, ForeignKey('users.id'), nullable=False)
     cover_id = Column("cover_id",Integer, ForeignKey('covers.id'), nullable=False)
     artist_id = Column("artist_id",Integer, ForeignKey('artists.id'), nullable=False)
     rating = Column("rating",Integer, nullable=False)
 
-    def _init__(self, user_id, cover_id, artist_id, rating):
+    def __init__(self, user_id, cover_id, artist_id, rating):
         self.user_id = user_id
         self.cover_id = cover_id
         self.artist_id = artist_id
