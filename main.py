0aa36a66085a91196726a16ee848da1c4c046113
Arthur Ribeiro <arthurfribeiro494@gmail.com>
2026-05-27 19:54:35 -0300

    Comentários adicionados

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import os
 
 load_dotenv()
 
+    # Utilização das variáveis de ambiente para criptografia de senha e JWT
+
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACESS_TOKEN_EXPIRE_MINUTES"))
@@ -16,7 +18,7 @@ ACESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACESS_TOKEN_EXPIRE_MINUTES"))
 app = FastAPI()
 
 
-
+    # CORS configurado para permitir requisições de qualquer origem, dividindo front e backend
 app.add_middleware(
     CORSMiddleware,
     allow_origins="*",
@@ -25,9 +27,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Instalar Argon2
+    # Criptografia de Senha e OAuth2
 bcrypt_context = CryptContext(schemes=["argon2"], deprecated="auto")
 oauth2_schme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+    # Importação das rotas de autenticação, reviews e álbuns, e inclusão no aplicativo FastAPI
 
 from auth_routes import auth_router
 from reviews_routes import review_router
