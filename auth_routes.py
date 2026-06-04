733088f45410a55298ace7ed41af5731cc60eeb9
Arthur Ribeiro <arthurfribeiro494@gmail.com>
2026-06-03 22:31:39 -0300

    Adicionando CRUD no auth

--- auth_routes.py
+++ auth_routes.py
@@ -3,7 +3,7 @@ from fastapi.security import OAuth2PasswordRequestForm
 from models import Users,db
 from dependencies import session_grab,verify_token
 from main import bcrypt_context, SECRET_KEY, ALGORITHM, ACESS_TOKEN_EXPIRE_MINUTES
-from schemas import UserSchema, loginSchema
+from schemas import UpdateUserSchema, UserSchema, loginSchema
 from jose import jwt, JWTError
 from datetime import datetime,timedelta,timezone 
 
@@ -82,3 +82,26 @@ async def refresh_token(user: Users = Depends(verify_token)):
             "token_type": "bearer",
             "user_id": user.id        
             }
+
+@auth_router.delete("/remove")
+async def remove_user(user: Users = Depends(verify_token), session = Depends(session_grab)):
+    session.delete(user)
+    session.commit()
+    return {"message": "Usuário removido com sucesso!"}
+
+@auth_router.patch("/update")
+async def update_user(upd_schema : UpdateUserSchema, user: Users = Depends(verify_token), session = Depends(session_grab)):
+    if upd_schema.username is not None:
+        user.username = upd_schema.username
+    if upd_schema.email is not None:
+        email_check = session.query(Users).filter(Users.email==upd_schema.email).first()
+        if email_check and email_check.id != user.id:
+            raise HTTPException(status_code=400, detail="Email já registrado por outro usuário.")
+        user.email = upd_schema.email
+    if upd_schema.senha is not None:
+        user.senha = bcrypt_context.hash(upd_schema.senha)
+    if upd_schema.pfp is not None:
+        user.pfp = upd_schema.pfp
+    session.commit()
+    return {"message": "Usuário atualizado com sucesso! username: " + user.username + " email: " + user.email + " pfp: " + str(user.pfp)}
+    
